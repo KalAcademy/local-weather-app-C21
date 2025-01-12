fbf11e5add878303cffebddcfffe7a8ff5dbf4d2
Kal <info@kalacademy.org>
2025-01-12 13:33:54 -0800

    Completed first component

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { ICurrentWeather } from '../icurrent-weather';
+import { WeatherService } from '../weather.service';
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-current-weather',
-  imports: [],
+  imports: [DatePipe, DecimalPipe],
   templateUrl: './current-weather.component.html',
   styleUrl: './current-weather.component.css'
 })
 export class CurrentWeatherComponent {
   current: ICurrentWeather = {
-    city: 'Redmond',
-    country: 'US',
+    city: '',
+    country: '',
     date: new Date(),
     image: '',
-    temperature: 41,
-    description: 'Mostly cloudy'
+    temperature: 0,
+    description: ''
+  }
+
+  constructor(private weatherService: WeatherService){
+    this.weatherService.getCurrentWeather('Venice', 'IT').subscribe(data => this.current = data)
   }
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { ICurrentWeatherData } from './icurrent-weather-data';
+import { ICurrentWeather } from './icurrent-weather';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +13,18 @@ export class WeatherService {
   constructor(private httpClient: HttpClient) { }
 
   getCurrentWeather(city: string, country: string){
-    this.httpClient.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=e0d96ab513fdd2b5997f40b86622ef81`)
+    return this.httpClient.get<ICurrentWeatherData>(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.appId}`).pipe(
+    map(data => this.transformToICurrentWeather(data)))
+  }
+
+  private transformToICurrentWeather(data: ICurrentWeatherData): ICurrentWeather{
+    return {
+      city: data.name,
+      country: data.sys.country,
+      date: new Date(data.dt*1000),
+      temperature: data.main.temp * 9/5 - 459.67,
+      description: data.weather[0].description,
+      image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    }
   }
 }
